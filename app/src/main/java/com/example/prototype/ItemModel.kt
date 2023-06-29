274ee7bf1fa1a89ed3fdee5010b9468586d513f7
earlratema <126766252+ST10089495@users.noreply.github.com>
2023-06-29 17:54:01 +0200

    Add items pages

---
+++ app/src/main/java/com/example/prototype/ItemModel.kt
@@ -0,0 +1,11 @@
+package com.example.prototype
+
+
+data class ItemModel(
+    var itemId: String? = null,
+    var itemName: String? = null,
+    var itemDescription: String? = null,
+    var itemDate: String? = null,
+
+
+    )
